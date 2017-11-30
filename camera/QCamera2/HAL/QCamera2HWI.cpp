650ca45f0bd8d04b26773819cfd06e86b85b970f
Suman Mukherjee <sumam@codeaurora.org>
2017-11-30 14:50:06 +0530

    QCamera2: Fixed compilation error for AOSP upgrade
      * Fixed compilation error for O
    
    Change-Id: I904ad9b6f6fa05a0b50816daada20944567d08bc

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -6308,6 +6308,7 @@ int32_t QCamera2HardwareInterface::processPrepSnapshotDoneEvent(
  *==========================================================================*/
 int32_t QCamera2HardwareInterface::processASDUpdate(cam_auto_scene_t scene)
 {
+#ifndef VANILLA_HAL
     size_t data_len = sizeof(cam_auto_scene_t);
     size_t buffer_len = 1 *sizeof(int)       //meta type
                       + 1 *sizeof(int)       //data len
@@ -6327,7 +6328,6 @@ int32_t QCamera2HardwareInterface::processASDUpdate(cam_auto_scene_t scene)
         return UNKNOWN_ERROR;
     }
 
-#ifndef VANILLA_HAL
     pASDData[0] = CAMERA_META_DATA_ASD;
     pASDData[1] = (int)data_len;
     pASDData[2] = scene;
